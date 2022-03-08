98149cb50f2eb9a6170c7e0120c31b0e875bbc97
Ankush Sharma <ankushsharma@Prabhavs-MacBook-Pro.local>
2022-03-08 15:58:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop/shop.component';
-const Hatspage=()=>(
+const Hatspage =Props =>{
+  console.log(Props);
+  return(
   <div>
-    <h1>HATS PAGE</h1>
+    <h1>HATS PAGE:</h1>
   </div>
-);
-
+  );
+  };
 function App() {
   return (
     <div>
       <Routes>
 
       <Route path='/' element={< HomePage />}/>
-      <Route path= '/hats' element={<Hatspage />}/>
-      <Route path= '/shoppage' element={<ShopPage />}/>
+      <Route exact path= '/hats' element={<Hatspage />}/>
+      <Route path= '/hats/shoppage' element={<ShopPage />}/>
 
       </Routes>
 </div>
