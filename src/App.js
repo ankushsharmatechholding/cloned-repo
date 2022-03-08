271bb6e28bb2b9cfdd0da5656b010ad929bf4b6a
Ankush Sharma <ankushsharma@Prabhavs-MacBook-Pro.local>
2022-03-08 13:04:24 +0530

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Route, Routes }from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
+const Hatspage=()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
+      <Routes>
 
-      <HomePage />
-    </div>
+      <Route path='/' element={< HomePage />}/>
+      <Route path= '/hats' element={<Hatspage />}/>
+      <Route path= '/shoppage' element={<ShopPage />}/>
+
+      </Routes>
+</div>
   );
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-//import './homepage.styles.scss';
+import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
@@ -11,3 +11,4 @@ const HomePage = () => (
 );
 
 export default HomePage;
+
