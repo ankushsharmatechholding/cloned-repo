d53a4d639461dd5d698a282df6bd87a6081fda01
Ankush Sharma <ankushsharma@Prabhavs-MacBook-Pro.local>
2022-03-10 15:35:47 +0530

    implemented firebase utils, included ability to store authentication users into your firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,30 @@ const config =
     appId: "1:477091646515:web:015eb4993afe3c77622746",
     measurementId: "G-CSGRV8QC6R"
   };
+export const createUserProfileDocument = async (userAuth, additionalData ) => {
+if(!userAuth) return;
 
+const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+const snapShot = await userRef.get();
+if(!snapShot.exists)
+{
+  const {displayName, email}= userAuth;
+  const createdAt = new Date();
+  try{
+    await userRef.set(
+      {
+        displayName,email,createdAt,...additionalData
+
+      }
+    )
+  }catch (error){
+    console.log('error creating user',error.message)
+  }
+}
+return userRef;
+
+}
   
   firebase.initializeApp(config);
 
--- src/pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+
 import SignIn from '/Users/ankushsharma/lesson-3-master/src/components/sign-in/sign-in.component';
-//import '.sign-in-and-sign-up.scss';
+import SignUp from "../../../components/sign-up/sign-up.component";
+
+
+import './sign-in-and-sign-up.scss';
 
 const SignInAndSignUpPage =() =>(
 <div className="Sign-in-and-sign-up">SIGN IN
 <SignIn />
+<SignUp />
 </div>
 );
 export default SignInAndSignUpPage;
